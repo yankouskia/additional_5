21dc63cdbb0ca2764d6ea95c88d224629e149075
asb1122 <asb1122@yandex.ru>
2017-09-17 18:45:26 +0300

    artem baturin: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var result = true;
+  for (var i = 0; i < bracketsConfig.length; i++) { // цикл по парам скобочек
+      var pair = bracketsConfig[i];
+      result = validateTextByPair(str, pair);
+      if (result === false) { break; }
+  }
+  return result;
 }
+
+function validateTextByPair(str, pair){
+  var openBracket = pair[0];
+  var closedBracket = pair[1];
+  console.log(openBracket +' '+ closedBracket);
+  var openBracketCount = 0;
+  var closedBracketCount = 0;
+  for (var i = 0; i < str.length; i++) {
+      if(str[i] == openBracket){
+        openBracketCount++;
+      }
+      if(str[i] == closedBracket){
+        closedBracketCount++;
+      }
+      if(openBracketCount < closedBracketCount){
+        break;
+      }
+  }
+  if(openBracketCount == closedBracketCount){
+    return true;
+  }else {
+    return false;
+  }
+}
+
+//check('()', [['(', ')']]);
